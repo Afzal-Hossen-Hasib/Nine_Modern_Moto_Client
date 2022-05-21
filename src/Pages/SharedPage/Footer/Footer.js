08a1dcad72dd90a5b87f1e866216a365ef05fbcb
afzalhossenhasib <ahhasib47@gmail.com>
2022-05-22 04:48:42 +0600

    footer added

--- src/Pages/SharedPage/Footer/Footer.js
+++ src/Pages/SharedPage/Footer/Footer.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import google from '../../../Images/app_logo/google.png'
 import ios from '../../../Images/app_logo/apple.png'
-import logo from '../../../Images/logo/logo.png'
+import logo from '../../../Images/footer/logo.png'
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div className='footer mt-5'>
+            <div className='container'>
             <div className="banner-container">
                 <div className="footer-first">
                     <h3>Download Our App</h3>
                     <p>Download Our App For ANDROID And IOS Mobile Phone</p>
                     <div className="app-logo">
-                        <img src={google} alt="" />
+                        <img className='me-2' src={google} alt="" />
                         <img src={ios} alt="" />
                     </div>
                 </div>
 
                 <div className="footer-second">
                     <img src={logo} alt="" />
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, perspiciatis!</p>
                 </div>
 
                 <div className="footer-third">
@@ -40,9 +42,11 @@ const Footer = () => {
                         <li>Youtube</li>
                     </ul>
                 </div>
+
             </div>
             <hr />
-            <p className='copyright'>Copyright 2022 - easy tutorials</p>
+            <p className='text-center'>Copyright 2022 - easy tutorials</p>
+            </div>
         </div>
     );
 };
